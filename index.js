19d557b048629fc7f2812da957a66823673f181a
NICOLAS SIGAL <nicolas.sigal@mercadolibre.com>
2019-05-07 14:08:43 -0300

    hello world with express

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', function (req, res) {
+    res.status(200).json({msg:'Hello World'})
+})
+
+app.listen(port, () => console.log(`App Listening to port ${port}`));
